83e4442891fa4f6e4a818831f48d42db804fe2ab
Scriptize <87991619+Scriptize@users.noreply.github.com>
2022-12-08 02:04:00 -0500

    Delete bot_config.py

--- bot_config.py
+++
@@ -1,5 +0,0 @@
-import hikari
-import lightbulb
-
-bot = lightbulb.BotApp(token='MTAwNjcxNzU1NzAwNjQwOTc4OQ.GpzEJD.U-nE7MFddrROwwRm1zZPXyq-NzeXmsEhlZnTF0',intents=hikari.intents.Intents.GUILD_MESSAGE_REACTIONS, default_enabled_guilds=(1006722736074264677))
-
